e0407bf6da5a51d050e5ef62f9c4ea1530a9c052
Maxime SERRETTA <maximeserretta+dev@gmail.com>
2024-04-10 10:09:57 +0200

    Creating the base of the architecture of the component Lesson and adding the files linked to the component

---
+++ client/src/components/Lesson/Lesson.jsx
@@ -0,0 +1,17 @@
+function Lesson(){
+    return (
+        <div className="lesson">
+            <h3 className="titreLesson">Lesson</h3>
+            <p className="lessonContent">lorem</p>
+            <h4>Quiz</h4>
+            <div className="quizLesson">
+                <p>quiz</p>
+            </div>
+            <div className="ressourcesVideo">
+                <p>video</p>
+            </div>
+        </div>
+    )
+}
+
+export default Lesson;
