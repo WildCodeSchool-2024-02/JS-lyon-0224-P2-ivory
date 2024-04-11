493ce11c2ec1cab6103b92d5cb4a86c5c4dc6c51
Mike69lyon <161336444+Mike69lyon@users.noreply.github.com>
2024-04-11 16:08:08 +0200

    formation.css

--- client/src/components/formation/Formation.jsx
+++ client/src/components/formation/Formation.jsx
@@ -1,10 +1,12 @@
 import PropTypes from "prop-types";
+import "./formation-style.css";
+
 
 function Formation({ name }) {
 
 
     return (
-    <div>
+    <div className="buttonFormations">
       {name.map((element, index) => (
         <button type="button" key={index.id}>{element.formation}</button>
       ))}
