95b68255fb8faf75e531a927e6ea9c071cd5106b
YohanGH <YohanGH@proton.me>
2024-04-12 09:56:49 +0200

    create folder code_editor and two files CodeEditor.jsx and CodeEditorStyle.module.css, And added npm packet for highlighter code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar/Navbar";
 import Lesson from "./components/Lesson/Lesson"
 import Footer from "./components/footer/Footer";
 import Formation from "./components/formation/Formation";
+import CodeEditor from "./components/code_editor/CodeEditor";
 
 const name = [
   { formation: "HTML" },
@@ -12,12 +13,21 @@ const name = [
   { formation: "Git" },
 ];
 
+const exempleDeCode = `
+function sayHello() {
+  console.log("Hello, world!");
+}
+`;
+
+const javascript = "javascript";
+
 function App() {
   return (
     <div>
       <Navbar/>
       <Lesson />
       <Formation name={name} />
+	  <CodeEditor language={javascript}  codeString={exempleDeCode} />
       <Footer />
     </div>
   );
